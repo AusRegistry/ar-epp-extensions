3b12dc49bc709fcd431d89d9dc36628201175cc5
Jem Mawson <jeremy.mawson@ausregistry.com.au>
2013-03-18 15:00:29 +1100

    publish the documents to a different location

--- build.tcl
+++ build.tcl
@@ -1,15 +1,15 @@
 #!/usr/bin/tclsh
 
 source xml2rfc.tcl
-set documents [exec find . -maxdepth 1 -type d -name "*-?.?" | cut -c3- ]
+set documents { application-1.0 idn-1.0 kv-1.0 premium-1.0 tmch-1.0 variant-1.1 }
 foreach {document} $documents {
    cd $document
    puts "processing $document to txt"
    xml2txt $document.xml
-   file rename -force $document.txt ../../ausregistry.github.com/epp-ext-doc/$document.txt
+   file rename -force $document.txt ../../ausregistry.github.com/doc/$document/$document.txt
    puts "processing $document to html"
    xml2html $document.xml
-   file rename -force $document.html ../../ausregistry.github.com/epp-ext-doc/$document.html
+   file rename -force $document.html ../../ausregistry.github.com/doc/$document/$document.html
    cd ..
 }
 exit
